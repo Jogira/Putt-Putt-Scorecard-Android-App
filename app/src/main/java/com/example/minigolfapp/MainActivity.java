de3d110c2fb41ffa27503a5482a705c9329dc5ab
Sage Thompson <sagethompson85@gmail.com>
2021-02-26 21:56:35 -0500

    active and history games can now be added to their respective tabs on the home page

--- app/src/main/java/com/example/minigolfapp/MainActivity.java
+++ app/src/main/java/com/example/minigolfapp/MainActivity.java
@@ -1,24 +1,34 @@
 package com.example.minigolfapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import de.hdodenhof.circleimageview.CircleImageView;
 
 public class MainActivity extends AppCompatActivity {
 
     private int currentGamePage = 0;
+    private int numActiveGames = 1;
+    private int numPastGames = 1;
     private static final int PAST_GAMES = 1;
     private static final int ACTIVE_GAMES = 0;
     private ImageButton statsButton;
     private CircleImageView settingsButton;
     private Button activeGamesButton;
     private Button pastGamesButton;
+    private LinearLayout gamesScrollViewContent;
+    private LinearLayout noGamesView;
+    private TextView noGamesText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,20 +39,35 @@ public class MainActivity extends AppCompatActivity {
         settingsButton = findViewById(R.id.settingsPageButton);
         activeGamesButton = findViewById(R.id.activeGamesButton);
         pastGamesButton = findViewById(R.id.pastGamesButton);
+        gamesScrollViewContent = findViewById(R.id.gamesScrollViewContent);
+        noGamesView = findViewById(R.id.noGamesView);
+        noGamesText = findViewById(R.id.noGamesText);
 
         activeGamesButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+
+                if(currentGamePage != ACTIVE_GAMES)
+                    populateGamesScrollView(ACTIVE_GAMES);
+
                 currentGamePage = ACTIVE_GAMES;
-                populateGamesScrollView(ACTIVE_GAMES);
+                activeGamesButton.getBackground().setTint(Color.parseColor("#2F2F2F"));
+                pastGamesButton.getBackground().setTint(Color.parseColor("#141414"));
+                noGamesText.setText("You have no active games :/");
             }
         });
 
         pastGamesButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+
+                if(currentGamePage != PAST_GAMES)
+                    populateGamesScrollView(PAST_GAMES);
+
                 currentGamePage = PAST_GAMES;
-                populateGamesScrollView(PAST_GAMES);
+                activeGamesButton.getBackground().setTint(Color.parseColor("#141414"));
+                pastGamesButton.getBackground().setTint(Color.parseColor("#2F2F2F"));
+                noGamesText.setText("Your history is empty :/");
             }
         });
 
@@ -59,6 +84,8 @@ public class MainActivity extends AppCompatActivity {
                 openSettingsPage();
             }
         });
+
+        populateGamesScrollView(ACTIVE_GAMES);
     }
 
     private void openStatsPage(){
@@ -74,9 +101,30 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void populateGamesScrollView(int pageType){
-        if(pageType != currentGamePage) {
 
+        gamesScrollViewContent.removeAllViews();
+
+        if(pageType == ACTIVE_GAMES && numActiveGames != 0 || pageType == PAST_GAMES && numPastGames != 0)
+            noGamesView.setVisibility(View.GONE);
+        else
+            noGamesView.setVisibility(View.VISIBLE);
+
+        if(pageType == ACTIVE_GAMES) {
+
+            //example active game
+            View exampleActiveGame = View.inflate(this, R.layout.item_active_view, null);
+            TextView currentHole = exampleActiveGame.findViewById(R.id.activeGameCurrentHoleTextView);
+            currentHole.setText("3");
+            gamesScrollViewContent.addView(exampleActiveGame);
         }
+        else {
+
+            //example history game
+            View examplePastGame = View.inflate(this, R.layout.item_history_view, null);
+            examplePastGame.setMinimumHeight(200);
+            gamesScrollViewContent.addView(examplePastGame);
+        }
+
     }
 
 }
