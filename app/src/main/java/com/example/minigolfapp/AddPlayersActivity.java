343d25296575ce3d5161f58739c23f6a625c0e33
Jonathan <Zweitonegoismus@gmail.com>
2021-02-27 21:23:40 -0500

    Player select screen +

--- app/src/main/java/com/example/minigolfapp/AddPlayersActivity.java
+++ app/src/main/java/com/example/minigolfapp/AddPlayersActivity.java
@@ -15,6 +15,14 @@ public class AddPlayersActivity extends AppCompatActivity
 {
         private Button backX;
         private Button createGame;
+        private ImageButton player1;
+        private ImageButton player2;
+        private ImageButton player3;
+        private ImageButton player4;
+        private ImageButton player5;
+        private ImageButton additionalPlayers;
+        private boolean flipped = false;
+
 
         @Override
         protected void onCreate(Bundle savedInstanceState) {
@@ -23,11 +31,20 @@ public class AddPlayersActivity extends AppCompatActivity
 
                 backX = findViewById(R.id.backX);
                 createGame = findViewById(R.id.createGame);
+                player1 = findViewById(R.id.player1Slot);
+                additionalPlayers = findViewById(R.id.additionalPlayer);
 
                 backX.setOnClickListener(new View.OnClickListener() {
                         @Override
                         public void onClick(View view) {
                                 goBackPage();
+                        }
+                });
+
+                player1.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View view) {
+                                flipPlayerIcon();
                         }
                 });
         }
@@ -36,5 +53,18 @@ public class AddPlayersActivity extends AppCompatActivity
                 Intent homePage = new Intent(this, MainActivity.class);
                 startActivity(homePage);
                 this.overridePendingTransition(R.anim.fade_out, R.anim.fade_in);
+        }
+
+        private void flipPlayerIcon(){
+                if (!flipped)
+                {
+                        player1.setBackgroundResource(R.drawable.rounded_background);
+                        flipped = true;
+                }
+                else
+                {
+                        player1.setBackgroundResource(R.drawable.sean_kingston_profile);
+                        flipped = false;
+                }
         }
 }
