c40a8b6dead6ecd358c40866f518981b84812a0e
Jonathan <Zweitonegoismus@gmail.com>
2021-02-27 19:21:52 -0500

    Create Game + Back Button

--- app/src/main/java/com/example/minigolfapp/AddPlayersActivity.java
+++ app/src/main/java/com/example/minigolfapp/AddPlayersActivity.java
@@ -3,6 +3,7 @@ package com.example.minigolfapp;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageButton;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -12,14 +13,28 @@ import de.hdodenhof.circleimageview.CircleImageView;
 
 public class AddPlayersActivity extends AppCompatActivity
 {
-
-        private ImageButton homeButton;
-        private CircleImageView settingsButton;
+        private Button backX;
+        private Button createGame;
 
         @Override
-        protected void onCreate(Bundle savedInstanceState)
-        {
+        protected void onCreate(Bundle savedInstanceState) {
                 super.onCreate(savedInstanceState);
                 setContentView(R.layout.add_players);
+
+                backX = findViewById(R.id.backX);
+                createGame = findViewById(R.id.createGame);
+
+                backX.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View view) {
+                                goBackPage();
+                        }
+                });
+        }
+
+        private void goBackPage(){
+                Intent homePage = new Intent(this, MainActivity.class);
+                startActivity(homePage);
+                this.overridePendingTransition(R.anim.fade_out, R.anim.fade_in);
         }
 }
