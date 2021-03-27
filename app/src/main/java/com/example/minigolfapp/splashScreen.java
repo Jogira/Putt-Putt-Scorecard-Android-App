c2dcff0dadfc394a823a382afcaf0fc465887670
Sage Thompson <sagethompson85@gmail.com>
2021-03-26 21:13:29 -0400

    fixed splashscreen transition animation

--- app/src/main/java/com/example/minigolfapp/splashScreen.java
+++ app/src/main/java/com/example/minigolfapp/splashScreen.java
@@ -12,20 +12,25 @@ public class splashScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
         final Handler handler = new Handler();
         final Runnable r = new Runnable() {
             public void run() {
                 toHome();
             }
         };
-        handler.postDelayed(r, 1000);
-
+        handler.postDelayed(r, 1200);
     }
 
     public void toHome(){
         Intent homeScreen = new Intent(this, MainActivity.class);
         startActivity(homeScreen);
-        this.overridePendingTransition(R.anim.fade_out, R.anim.fade_in);
+        this.overridePendingTransition(R.anim.activity_fade_out, R.anim.activity_fade_in);
     }
 
 }
